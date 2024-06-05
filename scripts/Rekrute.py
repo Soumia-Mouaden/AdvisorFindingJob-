b5bc71511487cfa888060e46f8d90ccff2baeb33
oumaima <oumaimaoutchakoucht@gmail.com>
2024-06-05 23:20:46 +0100

    bd

--- scripts/Rekrute.py
+++ scripts/Rekrute.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 import schedule
 import time
+from urllib.parse import urljoin
 
 def get_text_if_not_none(e):
     if e:
@@ -25,7 +26,8 @@ def scrape_and_store_data():
     liste_niveau_etude_demande = []
     liste_type_contrat = []
     liste_localisation = []
-
+    liste_competences = []
+    liste_url_img = []
     try:
         for page in range(1, 150):   
             url = f"{base_url}?page={page}"   
@@ -42,7 +44,6 @@ def scrape_and_store_data():
                 titleOfJob = get_text_if_not_none(titleOfJob_container.find("a"))
                 title = titleOfJob.split("|")
                 titleOfJob = title[0]
-                localisation = title[1]
                 holder = e_offre.find("div", class_="holder")
                 entreprise = get_text_if_not_none(holder.find_all("span")[0])
                 desc_poste = get_text_if_not_none(holder.find_all("span")[1])
@@ -60,6 +61,43 @@ def scrape_and_store_data():
                 type_contrat_propose_prec = liste_container.find_all("li")[4]
                 type_contrat_propose = get_text_if_not_none(type_contrat_propose_prec.find("a")) 
 
+                hreff = titleOfJob_container.find("a")
+                href_detail = hreff.get("href")
+                href_detaill = urljoin(base_url, href_detail)
+                responsee = requests.get(href_detaill, headers=headers)
+                responsee.raise_for_status()  
+                encoding = responsee.apparent_encoding
+                htmll = responsee.content.decode(encoding, errors='ignore')
+                soupp = BeautifulSoup(htmll, "html5lib") 
+
+                profil_texts = []
+                profil_section = soupp.find_all('div', class_='blc')
+                for div in profil_section:
+                    h2_element = div.find('h2')
+                    if h2_element and h2_element.text.strip() == 'Profil recherché :':
+                        content = div.find_all(['p', 'ul'])
+                        if content:
+                            for item in content:
+                                profil_texts.append(item.get_text(strip=True))
+
+                profil_recherche = "\n".join(profil_texts)
+                
+                address_span = soupp.find('span', id='address')
+                if address_span:
+                    localisation = address_span.text.strip()
+                else:
+                    if len(title)>1:
+                        localisation = title[1]
+                    else :
+                        localisation=""
+
+                image_entrep = soupp.find('img', class_='photo')
+                if image_entrep:
+                    url_img_prec = image_entrep.get("src")
+                    url_img = urljoin(base_url, url_img_prec)
+                else:
+                    url_img = ""
+
                 liste_titre.append(titleOfJob)
                 liste_desc_poste.append(desc_poste)
                 liste_entreprise.append(entreprise)
@@ -71,6 +109,8 @@ def scrape_and_store_data():
                 liste_niveau_etude_demande.append(niveau_etude_demande)
                 liste_type_contrat.append(type_contrat_propose)
                 liste_localisation.append(localisation)
+                liste_competences.append(profil_recherche)
+                liste_url_img.append(url_img)
 
     except requests.RequestException as e:
         print("Error:", e)
@@ -80,7 +120,7 @@ def scrape_and_store_data():
     with open(csv_filename, mode='w', newline='', encoding='UTF-8-SIG') as file:
         writer = csv.writer(file, delimiter=';')
         
-        writer.writerow(['Title','localisation', 'Company', 'Job Description', 'Start Date', 'End Date', 'Sector', 'Function', 'Experience Required', 'Education Level', 'Contract Type'])
+        writer.writerow(['Title','localisation', 'Company', 'Job Description', 'Start Date', 'End Date', 'Sector', 'Function', 'Experience Required', 'Education Level', 'Contract Type', 'compétences','image'])
         
         for i in range(len(liste_titre)):
             writer.writerow([
@@ -94,7 +134,9 @@ def scrape_and_store_data():
                 liste_fonction[i],
                 liste_exp_requise[i],
                 liste_niveau_etude_demande[i],
-                liste_type_contrat[i]
+                liste_type_contrat[i],
+                liste_competences[i],
+                liste_url_img[i]
             ])
 
     print(f"Data has been written to {csv_filename}")
