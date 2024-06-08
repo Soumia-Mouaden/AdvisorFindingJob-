d5b6949fc6a08e183f9c66e0abff554f0e6524b6
zahir-ikram <zahirikram09@gmail.com>
2024-06-08 12:30:29 +0200

    hhh

---
+++ env/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/inscription', methods=['GET', 'POST'])
+def inscription():
+    if request.method == 'POST':
+        # Logique pour gérer l'inscription
+        return redirect(url_for('home'))
+    return render_template('inscription1.html')
+
+@app.route('/connexion', methods=['GET', 'POST'])
+def connexion():
+    if request.method == 'POST':
+        # Logique pour gérer la connexion
+        return redirect(url_for('home'))
+    return render_template('connexion.html')
+
+@app.route('/recruteur_employee')
+def recruteur_employee():
+    return render_template('recruteur_employee.html')
+
+@app.route('/recruteur')
+def lien_vers_recruteur():
+    return render_template('lien_vers_recruteur.html')
+# Ajouter d'autres routes si nécessaire
+
+if __name__ == '__main__':
+    app.run(debug=True)
